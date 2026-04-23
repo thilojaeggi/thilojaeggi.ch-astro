93395ce76f55a57c852913771007f02a2c36e180
Thilo <thilo0123@gmail.com>
2026-04-24 01:58:28 +0200

    Improve accessibility

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Thilo Jaeggi';
-export const SITE_DESCRIPTION = 'Thilo Jaeggi - System Engineer';
+export const SITE_TITLE = "Thilo Jaeggi";
+export const SITE_DESCRIPTION =
+  "Thilo Jaeggi – DevOps & Software Engineer from Switzerland.";
